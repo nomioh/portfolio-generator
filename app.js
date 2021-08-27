a1232cc765bda66efeb9aa21c91eedf478cc1cb2
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-08-26 22:15:24 -0400

    install inquirer and questions

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template");
+
+const profileDataArgs = process.argv.slice(2);
+
 console.log(profileDataArgs);
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr);
-  }
-  console.log("================");
 
-  // is the same as this....
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+console.log(name, github);
 
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-};
+const pageHTML = generatePage(name, github);
+
+fs.writeFile("./index.html", pageHTML, (err) => {
+  if (err) throw err;
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
+
+var inquirer = require("inquirer");
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
